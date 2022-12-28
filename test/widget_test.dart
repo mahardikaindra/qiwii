58b7f7416d88512bcce3f6f5bae5604e3b8624d2
rakamahardika <rakamahardika22@gmail.com>
2022-12-28 16:26:33 +0700

    Testing codelabs

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,20 +11,35 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:qiwii/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
+  testWidgets('Codelab smoke test', (tester) async {
     await tester.pumpWidget(const MyApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    final textWidgets = tester.widgetList<Text>(find.byType(Text));
+    expect(textWidgets.length, greaterThan(2));
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+    expect(textWidgets.last.data, 'Startup Name Generator');
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    final wordPairRegExp = RegExp(r'^[A-Z]\w*[A-Z]\w*$');
+    final isWordPair = predicate<String>((s) => wordPairRegExp.hasMatch(s));
+    for (final widget in textWidgets.take(textWidgets.length - 1)) {
+      expect(widget.data, isWordPair);
+    }
+
+    final SemanticsHandle handle = tester.ensureSemantics();
+    await tester.pumpWidget(const MaterialApp(home: MyApp()));
+
+    // Checks that tappable nodes have a minimum size of 48 by 48 pixels for android.
+    await expectLater(tester, meetsGuideline(androidTapTargetGuideline));
+
+    // Checks that tappable nodes have a minimum size of 44 by 44 pixels for iOS.
+    await expectLater(tester, meetsGuideline(iOSTapTargetGuideline));
+
+    // Checks that touch targets with a tap or long press action are labeled.
+    await expectLater(tester, meetsGuideline(labeledTapTargetGuideline));
+
+    // Checks whether semantic nodes meet the minimum text contrast levels.
+    // The recommended text contrast is 3:1 for larger text (18 point and above regular)
+    await expectLater(tester, meetsGuideline(textContrastGuideline));
+    handle.dispose();
   });
 }
